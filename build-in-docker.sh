09db2176e4145d0132a92a2682191701743969a9
vvakame <vvakame+dev@gmail.com>
2017-08-02 22:38:09 +0900

    Revert "ビルド環境固定のためdocker imageにdigestを明示"
    This reverts commit bbc0cd7c3a464ef73e010bda60f24b9e7441200e.

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
 docker run -t --rm \
-  -v $(pwd):/book vvakame/review@sha256:dd4dcf3e5e4b2b317915bc5e1b4e07024ebc48129693c7eb675073ad86b70454 \
+  -v $(pwd):/book vvakame/review \
   /bin/bash -ci "cd /book && ./setup.sh && npm run pdf && ./grayscale.sh"
